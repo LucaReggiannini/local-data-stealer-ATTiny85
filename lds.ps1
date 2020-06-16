d3689b247eaa9ed19a5b7d8e8d1e55c311bbf125
LucaReggiannini <46603573+LucaReggiannini@users.noreply.github.com>
2020-06-16 12:28:47 +0200

    Update lds.ps1

--- lds.ps1
+++ lds.ps1
@@ -2,7 +2,7 @@
 $dsl = "External";
 $itefc = @('MicrosoftEdgeBackups','AppData','Musica');
 $ds = gwmi -Class Win32_logicaldisk | ? { $_.VolumeName -Like "*$dsl*" } | select DeviceID | % { $_.DeviceID };
-gci $home -Recurse -Exclude $itefc | cpp -Destination {Join-Path ($ds + "/" + $env:UserName + "_C/") $_.FullName.Substring($home.length)}
+gci $home -Recurse -Exclude $itefc | cpp -Destination -Force {Join-Path ($ds + "/" + $env:UserName + "_C/") $_.FullName.Substring($home.length)}
 $do = gwmi -Class Win32_logicaldisk | ? {  $_.DeviceID -NotLike "*C*" } | ? { $_.DeviceID -NotLike "*$ds*" } | select DeviceID | % { $_.DeviceID };
 $do | % { cpp -Path $_ -Destination ($ds + "/" + $env:UserName + "_" + $_).TrimEnd(':') -Recurse }
 [System.Windows.Forms.MessageBox]::Show("Process completed","LDS",0 ,48); 
