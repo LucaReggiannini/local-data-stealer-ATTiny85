65f9ea1345897d42c3c55aa33b1fe29fcb1c67e1
LucaReggiannini <46603573+LucaReggiannini@users.noreply.github.com>
2020-06-16 15:28:54 +0200

    Update lds.ps1

--- lds.ps1
+++ lds.ps1
@@ -5,4 +5,4 @@ $ds = gwmi -Class Win32_logicaldisk | ? { $_.VolumeName -Like "*$dsl*" } | selec
 gci $home -Recurse -Exclude $itefc | cp -Destination {Join-Path ($ds + "/" + $env:UserName + "_C/") $_.FullName.Substring($home.length)}
 $do = gwmi -Class Win32_logicaldisk | ? {  $_.DeviceID -NotLike "*C*" } | ? { $_.DeviceID -NotLike "*$ds*" } | select DeviceID | % { $_.DeviceID };
 $do | % { cp -Path $_ -Destination ($ds + "/" + $env:UserName + "_" + $_).TrimEnd(':') -Recurse }
-[System.Windows.Forms.MessageBox]::Show("Process completed","LDS",0 ,48); 
+[System.Windows.Forms.MessageBox]::Show("Process completed","LDS",0 ,48);
